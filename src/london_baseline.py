9a5fd20e3f16f04c7cbd0df3f8f9d26e79cd7f59
fadmemory <francisnash30@gmail.com>
2025-11-12 01:06:01 +0800

    (d)

--- src/london_baseline.py
+++ src/london_baseline.py
@@ -4,15 +4,27 @@
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
 
+import random
 import argparse
-import utils
+
+
+# random.seed(0)
 
 def main():
     accuracy = 0.0
-
+    count=0
+    bingo=0
     # Compute accuracy in the range [0.0, 100.0]
     ### YOUR CODE HERE ###
-    pass
+    for line,line2 in zip(open("birth_dev.tsv", encoding='utf-8'),open("vanilla.nopretrain.dev.predictions", encoding='utf-8')):
+        x = line.split('?')[1].strip()
+        y = line2
+        # print(x,y)
+        if x == "London":
+            count+=1
+            if y=="London":
+                bingo+=1
+    accuracy = (bingo/count)*100
     ### END YOUR CODE ###
 
     return accuracy
